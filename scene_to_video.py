54be46c524ab3d2543af4499b81e694655ff3cb7
ilay menahem <110230889+IlayMenahem@users.noreply.github.com>
2025-03-18 20:53:00 +0200

    update function calls

--- scene_to_video.py
+++ scene_to_video.py
@@ -1,42 +1,40 @@
 import os
 import bpy
 
-from utils import clear, clear_except_to
-from camera import add_camera, attach_camera_to_curve, get_video
+from utils import clear
+from camera import add_camera, get_video, look_at_curve
+from light import add_light
 from path import generate_curve
 
 if __name__ == "__main__":
     # initialize variables
-    scence_file_path = os.path.join(os.getcwd(), "scene.blend")
-    terrain_video_path = os.path.join(os.getcwd(), "videos", "terrain.mp4")
-    fire_only_video_path = os.path.join(os.getcwd(), "videos", "fire_only.mp4")
+    scence_file_path: str = os.path.join(os.getcwd(), "terrains", "forrestSimulationFinal.blend")
+    terrain_video_path: str = os.path.join(os.getcwd(), "videos", "terrain.mp4")
+    fire_only_video_path: str = os.path.join(os.getcwd(), "videos", "fire_only.mp4")
 
-    seed = 0
+    seed: int = 0
 
-    camera_location: tuple = (0, 0, 5)
-    camera_rotation: tuple = (0, 0, 0)
-    path_duration: int = 200
+    camera_location: tuple[float, float, float] = (0.0, 0.0, 50.0)
+    camera_rotation: tuple[float, float, float] = (0.0, 0.0, 0.0)
+    camera_fov: float = 20.0
+    camera_resolution: tuple[int, int] = (128, 128)
+    path_duration: int = 10
 
-    camera_x = 100
-    camera_y = 100
-    camera_z = 100
-    curve_offset: tuple = (camera_x, camera_y, camera_z)
-    curve_scale: float = 20
+    curve_offset: tuple[float, float, float] = (0.0, 0.0, 10.0)
+    curve_scale: float = 0
     point_count: int = 8
+
+    light_type: str = "SUN"
+    light_location: tuple[float, float, float] = (0.0, 0.0, 100.0)
+    light_strength: float = 10.0
+    light_color: tuple[float, float, float] = (1.0, 1.0, 1.0)
 
     # load scene
     clear()
     bpy.ops.wm.open_mainfile(filepath=scence_file_path)
-
-    # create camera and path
+    add_light(light_type, light_location, light_strength, light_color)
     camera = add_camera(camera_location, camera_rotation)
     curve = generate_curve(curve_offset, curve_scale, point_count, seed)
-    attach_camera_to_curve(camera, curve, path_duration)
+    look_at_curve(camera, curve, path_duration)
 
-    # render video
-    get_video(terrain_video_path, camera, path_duration)
-
-    # render fire only
-    fire_obj = bpy.data.objects["Fire"]
-    clear_except_to(fire_obj)
-    get_video(fire_only_video_path, camera, path_duration)
+    get_video(terrain_video_path, camera, path_duration, camera_resolution, camera_fov)
